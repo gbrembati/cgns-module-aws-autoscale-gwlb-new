621fdb486246035f31dc6e47eb2fcab76fcc5cc0
gbrembati <giorgio.brembati@gmail.com>
2023-12-14 16:19:52 +0100

    code alignment

--- main.tf
+++ main.tf
@@ -216,7 +216,7 @@ module "gwlb" {
   source  = "gbrembati/cgns-module-gwlb/aws"
   version = "1.0.1"
   
-  vpc_id = var.vpc_id
+  vpc_id     = var.vpc_id
   subnet_ids = var.gateways_subnets
 
   // --- General Settings ---
